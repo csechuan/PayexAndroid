42a6702b9a1c5ec5a172fb79307a6cb031db0aba
Raymond <csechuan@gmail.com>
2016-11-27 19:33:33 +0800

    link to sale page from email page

--- app/src/main/java/io/payex/android/ui/sale/EmailSlipActivity.java
+++ app/src/main/java/io/payex/android/ui/sale/EmailSlipActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.widget.Toolbar;
 
 import io.payex.android.R;
 import io.payex.android.ui.BaseActivity;
+import io.payex.android.ui.MainActivity;
 
-public class EmailSlipActivity extends BaseActivity {
+public class EmailSlipActivity extends BaseActivity
+implements EmailSlipFragment.OnFragmentInteractionListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,11 @@ public class EmailSlipActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setBackButton();
+    }
+
+    @Override
+    public void onSendButtonPressed() {
+        // fixme warning. anti-pattern. - single task on main activity?
+        startActivity(MainActivity.class, true);
     }
 }
--- app/src/main/java/io/payex/android/ui/sale/EmailSlipFragment.java
+++ app/src/main/java/io/payex/android/ui/sale/EmailSlipFragment.java
@@ -1,19 +1,53 @@
 package io.payex.android.ui.sale;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.payex.android.R;
 
 public class EmailSlipFragment extends Fragment {
 
+    private OnFragmentInteractionListener mListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email_slip, container, false);
+        View view = inflater.inflate(R.layout.fragment_email_slip, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.btn_send)
+    public void sendSlip() {
+        mListener.onSendButtonPressed();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onSendButtonPressed();
     }
 }
